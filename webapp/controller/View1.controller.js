a89d8ad92afa7d422523752647bc19e4b62422f4
kemaltsani <151552984+kemaltsani@users.noreply.github.com>
2025-12-16 11:30:32 +0700

    Add minimal View1 controller implementation

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("hr.executive.dashboard.controller.View1", {
+        /**
+         * Called when the controller is instantiated. Place your one-time initialization here.
+         */
+        onInit: function () {
+            // Initialization logic can be added here when needed
+        }
+    });
+});
